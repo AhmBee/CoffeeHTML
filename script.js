63e6c55d1067480c56a4161e72f993435186ae15
Ahmad Bilal <122690758+AhmBee@users.noreply.github.com>
2023-08-29 17:31:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var hnavbar = document.getElementById('hnavbar');
+
+
+function myFunction() {
+    
+    if (hnavbar.style.display === 'none'){
+        hnavbar.style.display = 'block';
+    } else {
+        hnavbar.style.display = 'none';
+    }
+    
+}
